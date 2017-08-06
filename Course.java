23c5889506b28aa5b7cebb4d13ff184bdd61ddbb
Julian Meyn <Ic3Venom@gmail.com>
2017-08-05 22:04:01 -0700

    mainly moved client functions into class

--- Course.java
+++ Course.java
@@ -1,21 +1,105 @@
-import java.io.Serializable;
+import java.io.*;
+import java.util.*;
 
 public class Course implements Serializable
 {
-
 	private String name;
 	private boolean weighted;
-	
-    public Course(String name, boolean weighted )
+	private List<List<String>> categories = new ArrayList<List<String>>();
+
+    public Course(String name, boolean weighted)
     {
     	this.name = name;
-    	this.weighted = weighted;
-    	
-    	
+		this.weighted = weighted;
+		
+		List<String> subCategory = new ArrayList<String>();
+		subCategory.add("WTF");
+		subCategory.add("Whyyy");
+		subCategory.add("TITLE");
+		categories.add(subCategory);
 	}
 
-	public String getName(){return name;}
-	public boolean getWeighted(){return weighted;}
-	public void setName(String name){this.name = name;}
-	public void setWeighted(boolean weighted){this.weighted = weighted;}
+    public static void writeCourse(Course c) throws IOException
+    {
+		//"fo" stands for FileOutput, "oo' stands for ObjectOutput
+		FileOutputStream foStream = new FileOutputStream(new File("fileOutput.txt"));
+		ObjectOutputStream ooStream = new ObjectOutputStream( foStream );
+
+		// Write object to file
+		ooStream.writeObject( c );
+
+		ooStream.close();
+		foStream.close();
+	}
+
+	public void readCategories()
+	{
+		
+		for(int i = 0; i < categories.size(); ++i)
+		{
+			List<String> subArray = categories.get(i);
+
+			for(int j = 0; j < subArray.size(); ++j)
+			{
+				System.out.print(subArray.get(j)+"  ");
+			}
+
+			System.out.println("");
+		}
+	}
+	public static void readCategory() throws IOException, ClassNotFoundException
+	{
+
+		//"fi" stands for FileInput, "oi" stands for ObjectInput
+		FileInputStream fiStream = new FileInputStream(new File("fileOutput.txt"));
+		ObjectInputStream oiStream = new ObjectInputStream( fiStream );
+
+		while( fiStream.available() > 0 )
+		{
+				
+			Course buffer = (Course) oiStream.readObject();
+			System.out.println( buffer.toString() );
+			
+		}
+			
+		oiStream.close();
+		fiStream.close();
+
+	}
+
+	public void writeCategory(String catName) throws IOException
+	{
+	}
+
+	public boolean categoryExists(String name)
+	{
+		return categories.indexOf(name) >= 0 ? true : false;
+	}
+	
+	public String getName()
+	{
+		return name;
+	}
+	
+	public boolean getWeighted()
+	{
+		return weighted;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public void setWeighted(boolean weighted)
+	{
+		this.weighted = weighted;
+	}
+
+	public String toString()
+	{
+		return "Course Name: " + name
+			+ "\nWeighted? " + weighted
+			+ "\nCategories: " + categories;
+	}
 }
--- client.java
+++ client.java
@@ -3,17 +3,13 @@
 */
 
 import java.util.Scanner;
-import java.io.*;
 
 public class client
 {
-	
     public static void main(String args[])
     {
     	String userInput;
     	Scanner sc = new Scanner(System.in);
-    	
-    	
     	do
     	{
     		System.out.print("What do you want to do? (type \'help\' for more info) > ");
@@ -22,20 +18,17 @@ public class client
     		switch(userInput.toLowerCase())
     		{
     		case "help":
-    		case "h":
-    			System.out.println("help!");
+			case "h":
+				help();
     			break;
     		case "new":
     		case "n":
-    			System.out.println("new!");
     			break;
     		case "check":
     		case "c":
-    			showAllGrades();
     			break;
     		case "all":
     		case "a":
-    			System.out.println("all!");
     			break;
     		case "q":
     		case "quit":
@@ -44,102 +37,26 @@ public class client
 				System.out.println("Unknown command \"" + userInput + "\". Try again.");
 			}
     	} while (!userInput.toLowerCase().equals("q") && !userInput.toLowerCase().equals("quit"));
-    	
     	sc.close();
     	
-    	
-    	
+		Course crs = new Course("CS38", true);
+		
+		System.out.println(crs.toString() + "\n");
+		crs.readCategories();
+		
+    	System.out.println("Terminating program.");
     	System.exit(0);
-    }
-    
-    private static void showAllGrades()
-    {
-    	
-    	return;
     }
     
     private static void help()
     {
-    	
-    	
-    }
-
-    private static void change(Course c)
-    {
-    	try
-        {
-            //"fo" stands for FileOutput, "oo' stands for ObjectOutput
-            FileOutputStream foStream = new FileOutputStream(new File("fileOutput.txt"));
-            ObjectOutputStream ooStream = new ObjectOutputStream( foStream );
-
-            // Write object to file
-            ooStream.writeObject( c );
-
-            ooStream.close();
-            foStream.close();
-
-        }
-        catch (FileNotFoundException e) 
-        {
-            
-            System.out.println("File not found");
-                
-        } 
-        catch (IOException e) 
-        {
-            
-            System.out.println("Error initializing stream");
-        
-        }
+		System.out.println("<grades help page>"
+			+ "\nAvailable commands:"
+			+ "\n\t'help' ('h'): shows the help page"
+			+ "\n\t'new [arg]' ('n [a]'): creates a new: [C]course/[]category/[G]grade"
+			+ "\n\t'all'  ('a'): shows all grades from all courses"
+			+ "\n\t'quit' ('q'): quits the program");
 	}
 	
-	private static void read()
-	{
-		try
-		{
-			//"fi" stands for FileInput, "oi" stands for ObjectInput
-			FileInputStream fiStream = new FileInputStream(new File("fileOutput.txt"));
-			ObjectInputStream oiStream = new ObjectInputStream( fiStream );
-
-			while( fiStream.available() > 0 )
-			{
-					
-				Course buffer = (Course) oiStream.readObject();
-				System.out.println( buffer.toString() );
-				
-			}
-				
-			oiStream.close();
-			fiStream.close();
-		}
-		catch (FileNotFoundException e) 
-        {
-            
-            System.out.println("File not found");
-                
-        } 
-        catch (IOException e) 
-        {
-            
-            System.out.println("Error initializing stream");
-        
-        } 
-        catch (ClassNotFoundException e) {
-        
-            e.printStackTrace();
-
-        }
-	}
-    
-    private static void classExists()
-    {
-    	
-    }
-    
-    private static void categoryExists()
-    {
-    	
-    }
-    
 }
 
