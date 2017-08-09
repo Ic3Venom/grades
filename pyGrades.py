98f595d97b5ca7bd5f9b47a0d3be973c3ae15fdd
Julian Meyn <Ic3Venom@gmail.com>
2017-08-08 22:47:38 -0700

    jupyter tests

--- pyGrades.py
+++ pyGrades.py
@@ -9,11 +9,36 @@ import pickle
 class Course:
 
     def __init__(self, name, weighted=True):
-        pass
+            self.categories = []
+            self.name = name
+            self.weighted = weighted
+
+    def addCategory(self, key, value={"Weight": 0}):
+        self.categories.update({key: value})
+
+    def addGrade(self, category, title, grade):
+        self.categories[category].update({title: grade})
+
+    def findCategory(self, category):
+        i = 0
+        for j in self.categories:
+            i += 1
+
+            if j == category:
+                return i
+
+        return -1
+
+    def __str__(self):
+        return "Name:       {0}\nWeighted?   {1}\nCategories: {2}" \
+               .format(self.name, self.weighted, self.categories)
 
 
 def main():
-    pass
+    crs = Course("CS38", False)
+
+    print(crs.__str__())
+
 
 if __name__ == '__main__':
     main()
