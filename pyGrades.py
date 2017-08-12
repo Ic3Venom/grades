82717b70f4d3299dd3da42c04c30a127ee261bf0
Julian Meyn <Ic3Venom@gmail.com>
2017-08-11 22:04:20 -0700

    static function findCourse work

--- pyGrades.py
+++ pyGrades.py
@@ -8,6 +8,7 @@ from difflib import get_close_matches
 
 
 class Course:
+    courses = []
 
     def __init__(self, name, weighted=False):
             self.categories = []
@@ -23,20 +24,53 @@ class Course:
             {title: [score, maxScore, percent]}
         )
 
-    def findCategory(self, category):
-        if category in self.categories:
-            return self.categories[category]
-        elif len(get_close_matches(category, self.categories.keys(), 5)) > 0:
-            print("Unknown category {0}".format(category))
-            print("Did you mean to type one of these words?")
+    def findCourse(userInput):
+        courseNames = []
+
+        for course in Course.courses:
+            courseNames.append(course.name)
+
+        if userInput in [x.lower() for x in courseNames]:
+            for course in Course.courses:
+                if course.name.lower() is userInput.lower():
+                    return course
+        elif len(get_close_matches(userInput,
+                [x.lower() for x in courseNames])) > 0:
+
+            print("Unknown course {0}".format(userInput))
+            print("Did you mean to type one of these courses?")
 
             for i, j in enumerate(
-                    get_close_matches(category, self.categories.keys(), 5)):
+                    get_close_matches(userInput, 
+                        [x.lower() for x in courseNames], 5)):
                 print("{0}: {1}".format(i+1, j))
 
             return None
         else:
-            print("The word '{0}' does not exist. Try again".format(category))
+            print("The course name '{0}' does not exist. Try again"
+                  .format(userInput))
+            return None
+
+    def findCategory(userInput):
+        userInput = userInput.lower()
+        keys = []
+
+        for i in self.categories:
+            keys.append(i.lower())
+
+        if category in self.categories:
+            return self.categories[category]
+        elif len(get_close_matches(userInput, self.categories.keys(), 5)) > 0:
+            print("Unknown category {0}".format(category))
+            print("Did you mean to type one of these categories?")
+
+            for i, j in enumerate(
+                    get_close_matches(userInput, self.categories.keys(), 5)):
+                print("{0}: {1}".format(i+1, j))
+
+            return None
+        else:
+            print("The word '{0}' does not exist. Try again".format(userInput))
             return None
 
     def __str__(self):
@@ -48,15 +82,20 @@ class Course:
 
 
 def main():
-    crs = Course("CS38")
-    crs.addCategory("Tests", 50)
-    crs.addGrade("Tests", "test1", 100, 100)
-    crs.addCategory("Quizzes", 20)
-    crs.addGrade("Quizzes", "popquiz1", 20, 22)
-    crs.addCategory("Homework", 10)
-    crs.addGrade("Homework", "assignment1", 10, 10)
-    print(crs.__str__())
+    Course.courses.append(Course("CS38"))
+    Course.courses[0].addCategory("Tests", 50)
+    Course.courses[0].addGrade("Tests", "test1", 100, 100)
+    Course.courses[0].addCategory("Quizzes", 20)
+    Course.courses[0].addGrade("Quizzes", "popquiz1", 20, 22)
+    Course.courses[0].addCategory("Homework", 10)
+    Course.courses[0].addGrade("Homework", "assignment1", 10, 10)
 
+    validCategory = None
+    while validCategory is None:
+        print("What course do you want to know about?")
+        userInput = input(">>> ")
+        validCategory = Course.findCourse(userInput.lower())
 
+    print(validCategory.__str__())
 if __name__ == '__main__':
     main()
